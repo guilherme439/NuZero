b239bf219c8561f165425ee0642dcd5c6d280d15
guilherme439 <guilherme.palma@tecnico.ulisboa.pt>
2023-10-09 15:44:24 +0100

    more ilu configs

--- Run.py
+++ Run.py
@@ -150,7 +150,7 @@ def main():
             case 1: # Run on local machine
                 
                 game_class = SCS_Game
-                game_args = ["SCS/Game_configs/randomized_config.yml"]
+                game_args = ["SCS/Game_configs/mirrored_config.yml"]
                 game = game_class(*game_args)
 
                 alpha_config_path="Configs/Config_Files/Training/local_training_config.ini"
@@ -252,7 +252,7 @@ def main():
             case 5: # Run with debug set
                 
                 game_class = SCS_Game
-                game_args = ["SCS/Game_configs/mirrored_config.yml"]
+                game_args = ["SCS/Game_configs/randomized_config.yml"]
                 game = game_class(*game_args)
 
                 alpha_config_path="Configs/Config_Files/Training/local_training_config.ini"
