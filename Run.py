5d3e2d1d959e8c745092d607ec54e254af421372
guilherme439 <guilherme.palma@tecnico.ulisboa.pt>
2024-01-17 00:54:35 +0000

    presets and configs

--- Run.py
+++ Run.py
@@ -133,12 +133,12 @@ def main():
             case 1: # Continue training
                 
                 game_class = SCS_Game
-                game_args_list = [ ["SCS/Game_configs/randomized_config_5.yml"]]
+                game_args_list = [ ["SCS/Game_configs/mirrored_config_5.yml"]]
                 
                 game = game_class(*game_args_list[0])
 
-                trained_network_name = "randomized_ilu_c"
-                continue_network_name = "randomized_ilu_c2"
+                trained_network_name = "mirror_lower_lr"
+                continue_network_name = "mirror_lower_lr_c"
                 iteration = "auto"
                 use_same_configs = False
                 curriculum_learning = False
@@ -1443,16 +1443,16 @@ def continue_training(game_class, game_args_list, trained_network_name, continue
 
     pickle_path =  trained_model_folder_path + "base_model.pkl"
 
-    '''
+    #'''
     with open(pickle_path, 'rb') as file:
         model = pickle.load(file)
-    '''
+    #'''
 
-    #'''    
+    '''    
     in_channels = game.get_state_shape()[0]
     policy_channels = game.get_action_space_shape()[0]
     model = RecurrentNet(in_channels, policy_channels, 256, 2, recall=True, policy_head="conv", value_head="reduce", value_activation="relu", hex=True)
-    #'''
+    '''
 
     model_paths = glob.glob(trained_model_folder_path + "*_model")
     
