95f7ac434a927b80b19614f71d43034097aa0c0b
guilherme439 <guilherme.palma@tecnico.ulisboa.pt>
2024-02-06 20:40:38 +0000

    presets for graphs

--- Run.py
+++ Run.py
@@ -859,27 +859,129 @@ def main():
             case 9: # build graphs from data
                 
                 figname = "30_vs_res"
-                titulo = "Extrapolation Solo"
-                plt.figure(figsize=(15, 10))
+                titulo = "Architecture Comparison"
+                plt.figure(figsize=(12, 6.8))
 
                 data_path = "Graphs/_graph_data/solo_reduce_prog_4_1100_0-100-iterations_extrapolation.pkl"
-                win_rates = pickle_load(data_path)
+                prog_win_rates = pickle_load(data_path)
 
                 data_path = "Graphs/_graph_data/solo_res_5x5_to_10x10_win_rates.pkl"
                 res_wr = pickle_load(data_path)
 
-                for size_i in range(len(win_rates)):
-                    iterations = win_rates[size_i]
+                for size_i in range(len(prog_win_rates)):
+                    iterations = prog_win_rates[size_i]
                     line_lst = plt.plot(range(len(iterations)), iterations, label = str(size_i+5) + "x" + str(size_i+5))
                     color = line_lst[0].get_color()
                     wr = res_wr[size_i]
                     plt.axhline(y=wr, linestyle='--', label = str(size_i+5) + "x" + str(size_i+5) + "_ResNet", color = color)
 
+                plt.xlabel("Recurrent Iterations")
+                plt.ylabel("Win Ratio")
+        
+                plt.title(titulo, pad=20, fontsize = 14)
+
+                lgd = plt.legend(bbox_to_anchor=(1,1))
+                plt.gcf().canvas.draw()
+                invFigure = plt.gcf().transFigure.inverted()
+
+                lgd_pos = lgd.get_window_extent()
+                lgd_coord = invFigure.transform(lgd_pos)
+                lgd_xmax = lgd_coord[1, 0]
+
+                ax_pos = plt.gca().get_window_extent()
+                ax_coord = invFigure.transform(ax_pos)
+                ax_xmax = ax_coord[1, 0]        
+
+                shift = 1.1 - (lgd_xmax - ax_xmax)
+                plt.gcf().tight_layout(rect=(0, 0, shift, 1))
+
+
+                plt.savefig("Graphs/" + figname, dpi=300)
+                plt.clf()
+
+                return
+            
+            case 10: # build more graphs from data
                 
-                plt.title(titulo)
-                plt.legend(bbox_to_anchor=(1,1))
-                #plt.tight_layout()
-                plt.savefig("Graphs/" + figname)
+                figname = "solo_extrapolation"
+                titulo = "Solo Soldier Extrapolation"
+                plt.figure(figsize=(12, 7))
+
+                data_path = "Graphs/_graph_data/solo_final_1100_0-100-iterations.pkl"
+                prog_win_rates = pickle_load(data_path)
+
+                for size_i in range(len(prog_win_rates)):
+                    iterations = prog_win_rates[size_i]
+                    line_lst = plt.plot(range(len(iterations)), iterations, label = str(size_i+5) + "x" + str(size_i+5))
+        
+
+                plt.xlabel("Recurrent Iterations")
+                plt.ylabel("Win Ratio")
+        
+                plt.title(titulo, pad=20, fontsize = 14)
+
+                lgd = plt.legend(bbox_to_anchor=(1,1))
+                plt.gcf().canvas.draw()
+                invFigure = plt.gcf().transFigure.inverted()
+
+                lgd_pos = lgd.get_window_extent()
+                lgd_coord = invFigure.transform(lgd_pos)
+                lgd_xmax = lgd_coord[1, 0]
+
+                ax_pos = plt.gca().get_window_extent()
+                ax_coord = invFigure.transform(ax_pos)
+                ax_xmax = ax_coord[1, 0]        
+
+                shift = 1.05 - (lgd_xmax - ax_xmax)
+                plt.gcf().tight_layout(rect=(0, 0, shift, 1))
+
+
+                plt.savefig("Graphs/" + figname, dpi=300)
+                plt.clf()
+
+                return
+            
+            case 11: # build even more graphs from data
+                
+                figname = "30_vs_res"
+                titulo = "Architecture Comparison"
+                plt.figure(figsize=(12, 6.8))
+
+                data_path = "Graphs/_graph_data/solo_reduce_prog_4_1100_0-100-iterations_extrapolation.pkl"
+                prog_win_rates = pickle_load(data_path)
+
+                data_path = "Graphs/_graph_data/solo_res_5x5_to_10x10_win_rates.pkl"
+                res_wr = pickle_load(data_path)
+
+                for size_i in range(len(prog_win_rates)):
+                    iterations = prog_win_rates[size_i]
+                    line_lst = plt.plot(range(len(iterations)), iterations, label = str(size_i+5) + "x" + str(size_i+5))
+                    color = line_lst[0].get_color()
+                    wr = res_wr[size_i]
+                    plt.axhline(y=wr, linestyle='--', label = str(size_i+5) + "x" + str(size_i+5) + "_ResNet", color = color)
+
+                plt.xlabel("Recurrent Iterations")
+                plt.ylabel("Win Ratio")
+        
+                plt.title(titulo, pad=20, fontsize = 14)
+
+                lgd = plt.legend(bbox_to_anchor=(1,1))
+                plt.gcf().canvas.draw()
+                invFigure = plt.gcf().transFigure.inverted()
+
+                lgd_pos = lgd.get_window_extent()
+                lgd_coord = invFigure.transform(lgd_pos)
+                lgd_xmax = lgd_coord[1, 0]
+
+                ax_pos = plt.gca().get_window_extent()
+                ax_coord = invFigure.transform(ax_pos)
+                ax_xmax = ax_coord[1, 0]        
+
+                shift = 1.1 - (lgd_xmax - ax_xmax)
+                plt.gcf().tight_layout(rect=(0, 0, shift, 1))
+
+
+                plt.savefig("Graphs/" + figname, dpi=300)
                 plt.clf()
 
                 return
